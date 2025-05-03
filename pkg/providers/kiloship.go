cdf421153879dc647c0c1c149aa06d46425e5778
ManhPineapple <aktaovipma@gmail.com>
2025-05-03 19:23:47 +0700

    Disable Kiloship trackinfo

--- pkg/providers/kiloship.go
+++ pkg/providers/kiloship.go
@@ -60,6 +60,8 @@ func (c *KiloshipCarrier) CreateLabel(in RequestCreateLabel) (*ResponseCreateLab
 }
 
 func (c *KiloshipCarrier) TrackInfo(trackingNumber string) ([]ResponseTrack, error) {
+	return nil, errors.New("Feature is disabled!")
+
 	res, err := c.Service.TrackingLabel(trackingNumber)
 	if err != nil {
 		return nil, err
