94b628a953965bc7443454f91e3547a071c1c6dc
ManhPineapple <aktaovipma@gmail.com>
2025-09-13 15:45:38 +0700

    fix: warehouse get package by ordernum

--- pkg/sqlmanager/package_manager.go
+++ pkg/sqlmanager/package_manager.go
@@ -162,8 +162,17 @@ func (m PackageManager) BuildPackageCodeQuery(opts PackageCodeQueryOption) *gorm
 
 	if opts.Code != "" {
 		db = db.Joins("JOIN packages on packages.package_code_id = package_codes.id")
-		db = db.Where("package_codes.code = ? OR packages.id = (?) OR packages.custom_cn_barcode = ? OR packages.order_number = ?", opts.Code, m.db.Model(&entity.Tracking{}).
-			Select("package_id").Limit(1).Where("trackings.tracking_number = ? AND trackings.status != ?", opts.Code, constant.TrackingStatusCanceled), opts.Code, opts.Code)
+		db = db.Joins("LEFT JOIN trackings ON trackings.package_id = packages.id").
+			Where("trackings.status != ?", constant.TrackingStatusCanceled).
+			Where("(package_codes.code = ? OR packages.id = (?) OR packages.custom_cn_barcode = ? OR packages.order_number = ?)",
+				opts.Code,
+				m.db.Model(&entity.Tracking{}).
+					Select("package_id").
+					Limit(1).
+					Where("tracking_number = ?", opts.Code),
+				opts.Code,
+				opts.Code,
+			)
 	}
 
 	if len(opts.SearchCode) > 0 {
