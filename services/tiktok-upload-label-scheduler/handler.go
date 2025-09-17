89401b479c43e58ce82c081b9391e71dac0d6239
ManhPineapple <aktaovipma@gmail.com>
2025-09-17 09:53:32 +0700

    increase date range to upload tiktok label

--- services/tiktok-upload-label-scheduler/handler.go
+++ services/tiktok-upload-label-scheduler/handler.go
@@ -46,11 +46,11 @@ func (h *ShipmentTrackingHandler) Process() {
 }
 
 func (h *ShipmentTrackingHandler) uploadTiktokLabel() error {
-	twelveHoursAgo := time.Now().Add(-12 * time.Hour).Format("2006-01-02 15:04:05")
+	start := time.Now().AddDate(0, 0, -14).Format(time.RFC3339)
 
 	tiktokPkgs, err := h.PackageManager.GetPackages(sqlmanager.PackageQueryOption{
 		HasTiktokLabel: true,
-		StartDate:      twelveHoursAgo,
+		StartDate:      start,
 	})
 	if err != nil {
 		return err
