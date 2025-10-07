dffd35c81b679fae8e3b44fe2759c61222b37e60
ManhPineapple <manh.tv0911@gmail.com>
2025-10-07 17:31:53 +0700

    admin export package more field

--- services/api/handlers/admin/export_handler.go
+++ services/api/handlers/admin/export_handler.go
@@ -206,12 +206,13 @@ func (h *ExportHandler) ExportShipment() gin.HandlerFunc {
 }
 func (h *ExportHandler) exportCsvPackageXlsx(packages []entity.Package, userID int64) (string, error) {
 	header := []string{
-		"AB tracking",
-		"Last mile tracking",
+		"Mã AB",
+		"Mã tracking",
 		"Mã đơn hàng",
 		"Chi tiết hàng hóa",
 		"Mã lô",
 		"Ngày tạo",
+		"Ngày chấp nhận",
 		"Tên khách hàng",
 		"Tên người nhận",
 		"SĐT người nhận",
@@ -233,32 +234,26 @@ func (h *ExportHandler) exportCsvPackageXlsx(packages []entity.Package, userID i
 	}
 
 	f1 := excelize.NewFile()
+	sheetName := "Sheet1"
 
+	// Write header row
 	for index, item := range header {
 		colStr := fmt.Sprintf("%s1", AdmintoCharStr(index+1))
-		err := f1.SetCellValue("Sheet1", colStr, item)
-		if err != nil {
+		if err := f1.SetCellValue(sheetName, colStr, item); err != nil {
 			h.Logger.Errorf("Write xlsx header error: %v", err)
 		}
 	}
 
-	fileName := fmt.Sprintf("danh_sach_van_don_%d_%02d_%02d.xlsx", time.Now().Day(), int(time.Now().Month()), time.Now().Year())
-	f, err := os.Create(fileName)
-	if err != nil {
-		h.Logger.Errorf("Create file tmp: %v", err)
-		return "", err
-	}
-	defer f.Close()
-	defer os.Remove(fileName)
-	writer := csv.NewWriter(f)
-	defer writer.Flush()
+	fileName := fmt.Sprintf("danh_sach_van_don_%d_%02d_%02d.xlsx",
+		time.Now().Day(), int(time.Now().Month()), time.Now().Year(),
+	)
 
 	for indexRow, Package := range packages {
 		var trackingNumber string
 		if Package.Tracking != nil {
 			trackingNumber = Package.Tracking.TrackingNumber
 		}
-		tmp := []string{}
+
 		pkgCode := ""
 		if Package.PackageCode != nil {
 			pkgCode = Package.PackageCode.Code
@@ -268,12 +263,12 @@ func (h *ExportHandler) exportCsvPackageXlsx(packages []entity.Package, userID i
 			pkgCode = ""
 		}
 
-		tmp = append(tmp, []string{
+		tmp := []string{
 			pkgCode,
 			trackingNumber,
 			Package.OrderNumber,
 			Package.Detail,
-		}...)
+		}
 
 		// lấy mã lô
 		options := sqlmanager.ShipmentQueryOptions{
@@ -281,11 +276,13 @@ func (h *ExportHandler) exportCsvPackageXlsx(packages []entity.Package, userID i
 			PackageID:     Package.ID,
 		}
 
-		var shipmentNumber = ""
+		shipmentNumber := ""
 		shipment, _ := h.ShipmentManager.GetShipment(options)
 		if shipment != nil {
 			shipmentNumber = cast.ToString(shipment.ID)
 		}
+
+		// tính phí phát sinh
 		var extraFee float64
 		for _, fee := range Package.ExtraFee {
 			extraFee += fee.Amount
@@ -299,9 +296,15 @@ func (h *ExportHandler) exportCsvPackageXlsx(packages []entity.Package, userID i
 			}
 		}
 
+		var checkinTimeStr string
+		if Package.CheckinWarehouseAt != nil {
+			checkinTimeStr = Package.CheckinWarehouseAt.Add(7 * time.Hour).Format("2006-01-02T15:04:05")
+		}
+
 		tmp = append(tmp, []string{
 			shipmentNumber,
 			cast.ToString(Package.CreatedAt.Add(7 * time.Hour).Format("2006-01-02T15:04:05")),
+			cast.ToString(checkinTimeStr),
 			Package.User.FullName,
 			Package.Recipient,
 			Package.PhoneNumber,
@@ -324,33 +327,35 @@ func (h *ExportHandler) exportCsvPackageXlsx(packages []entity.Package, userID i
 
 		for indexCol, item := range tmp {
 			cellStr := fmt.Sprintf("%s%d", toCharStr(indexCol+1), indexRow+2)
-			err := f1.SetCellValue("Sheet1", cellStr, item)
-			if err != nil {
+			if err := f1.SetCellValue(sheetName, cellStr, item); err != nil {
 				h.Logger.Errorf("Write xlsx row %v column %v error: %v", indexRow+2, indexCol+1, err)
 			}
 		}
 	}
 
+	// Save the file locally
 	if err := f1.SaveAs(fileName); err != nil {
-		h.Logger.Errorf("read file error: %v/n", err)
+		h.Logger.Errorf("save file error: %v", err)
 		return "", err
 	}
 
-	filePath := fmt.Sprintf("export/package/%s/%s%s", viper.GetString("env"), file.MakeUploadUserIDPath(userID, ""), fileName)
+	filePath := fmt.Sprintf("export/package/%s/%s%s",
+		viper.GetString("env"),
+		file.MakeUploadUserIDPath(userID, ""),
+		fileName,
+	)
 
 	buffer, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		h.Logger.Errorf("read file error: %v/n", err)
+		h.Logger.Errorf("read file error: %v", err)
 		return "", err
 	}
-	f.Read(buffer)
+	defer os.Remove(fileName)
 
 	contentType := constant.ContentTypeXlsxExcel2007Above
 	bucketExport := viper.GetString("bucket.export_packages")
-	err = h.S3.UploadFile(bytes.NewBuffer(buffer), filePath, bucketExport, contentType)
-
-	if err != nil {
-		h.Logger.Errorf("read file error: %v/n", err)
+	if err := h.S3.UploadFile(bytes.NewBuffer(buffer), filePath, bucketExport, contentType); err != nil {
+		h.Logger.Errorf("upload file error: %v", err)
 		return "", err
 	}
 
